8d423748cba9dd7e0c244fc01bcbf8337a71b5d6
aman1357 <amancorp04@gmail.com>
2024-01-19 23:55:00 +0530

    .

---
+++ app/components/auth/signup/Page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Signup() {
+  return (
+    <div>Signup
+     <div>
+        <input type='email' placeholder='Email'/>
+        <input type="password" placeholder="Password"/>
+        <button>Signup</button>
+     </div>
+
+    </div>
+  )
+}
+
+export default Signup
