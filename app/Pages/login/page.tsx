a702732721e5e22522c9dcac76fae859b7138292
aman1357 <amancorp04@gmail.com>
2024-03-11 23:58:39 +0530

    .

--- app/Pages/login/page.tsx
+++ app/Pages/login/page.tsx
@@ -1,10 +1,13 @@
+"use client"
 import React from 'react'
 import SignIn from '@/app/components/auth/Login/Page'
+import SpotifyAuth from '@/app/components/auth/SpotifyAuth/page'
 
 function page() {
   return (
     <div>
          <SignIn/>
+         <SpotifyAuth/>
     </div>
   )
 }
