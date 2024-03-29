3bef2dfb2d99695ada4e30f77b7843c394395b4a
aman1357 <amancorp04@gmail.com>
2024-03-29 12:58:56 +0530

    .

---
+++ app/utils/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBUWxk0YoTodAAflUMyvlJlB-p9xk4sM4g",
+  authDomain: "lyriclink-2e3d6.firebaseapp.com",
+  projectId: "lyriclink-2e3d6",
+  storageBucket: "lyriclink-2e3d6.appspot.com",
+  messagingSenderId: "1031045986513",
+  appId: "1:1031045986513:web:7a9b01f5653db55e50adf0",
+  measurementId: "G-PJY350YF3R"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
