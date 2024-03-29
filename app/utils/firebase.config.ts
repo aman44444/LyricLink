c321a63f6fce6dd4dcc9e18e0f5f05d356961d1e
aman1357 <amancorp04@gmail.com>
2024-03-29 12:58:56 +0530

    .

---
+++ app/utils/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  ***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
