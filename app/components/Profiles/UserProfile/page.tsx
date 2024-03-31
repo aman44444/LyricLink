4fea1dd67b2281403edb0b459bcfee9ba182309a
aman1357 <amancorp04@gmail.com>
2024-03-31 23:49:29 +0530

    .

--- app/components/Profiles/UserProfile/page.tsx
+++ app/components/Profiles/UserProfile/page.tsx
@@ -1,30 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import { fetchUserData } from "@/app/utils/spotifyAPI";
-
-
-
-// const UserProfile: React.FC = () => {
-//   const [userData, setUserData] = useState<any>(null);
-//   useEffect(() => {
-
-//     fetchUserData()
-//         .then(data => setUserData(data))
-//         .catch(error => console.error('Error fetching user data:', error));
-   
-    
-// }, []);
-
-//   return (
-//     <div>
-//       <h2>Welcome, {userData?.display_name}</h2>
-//       <p>Email: {userData?.email}</p>
-      
-//     </div>
-//   );
-// };
-
-// export default UserProfile;
-// UserProfile.tsx
 import React, { useEffect, useState } from "react";
 import { fetchUserData } from "@/app/utils/spotifyAPI";
 import { firestore } from "@/app/utils/firebase.config";
