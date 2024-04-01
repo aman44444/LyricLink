f2c0ff45993e82b25498f87fb8706b1e5a24d528
aman1357 <amancorp04@gmail.com>
2024-04-01 23:30:40 +0530

    .

--- app/components/Dating/MatchCard/Page.tsx
+++ app/components/Dating/MatchCard/Page.tsx
@@ -7,13 +7,21 @@ interface CardProps {
 
 const MatchCard: React.FC<CardProps> = ({ user }) => {
   return (
-    <div className="card">
-      {user.images && <img src={user.images[0]?.url} alt="Profile" />}
-      <div className="card-body">
-        <h5 className="card-title">{user.display_name}</h5>
+    // <div className="card">
+    //   {user.images && <img src={user.images[0]?.url} alt="Profile" />}
+    //   <div className="card-body">
+    //     <h5 className="card-title">{user.display_name}</h5>
         
-      </div>
+    //   </div>
+    // </div>
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden">
+    { user.images && <img className="w-full h-64 object-cover" src={user.images[0]?.url} alt="Profile"  />}
+    <div className="p-6">
+      <h2 className="text-2xl font-semibold mb-2">{user.display_name}</h2>
+      
+      
     </div>
+  </div>
   );
 };
 
