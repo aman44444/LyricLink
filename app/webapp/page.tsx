dce0962ee7703e971ca520161fbb1562e0d868d9
aman1357 <amancorp04@gmail.com>
2024-04-02 00:50:06 +0530

    .

--- app/webapp/page.tsx
+++ app/webapp/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Layout from "../components/Home/Layout/Page";
 import styles from "./Button.module.css"
+import { FaLink } from "react-icons/fa6";
 
 const CLIENT_ID = "51ab00be48604869a24fa74a4be50ddb"; 
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
@@ -74,10 +75,13 @@ const WebApp: React.FC = () => {
         <Layout/>
         </>
       ) : (
-        <div className="flex justify-center items-center w-screen h-screen  bg-neutral-800 ">
-          <div className="w-1/4 h-1/2 border-2 flex items-center  flex-col bg-black rounded-xl border-none">
-          <h2 className="text-white m-10 text-3xl font-bold">LyricLink</h2>
-           <button className="w-32 h-12 border-none bg-green-700 absolute top-2/4 text-white rounded-xl" onClick={handleLogin}>Login to Spotify</button>
+        <div className="flex justify-center items-center w-screen h-screen  bg-green-800 ">
+          <div className="w-1/4 h-1/2 border-2 flex items-center  flex-col bg-green-800 rounded-xl border-none">
+          <div className="flex m-10  items-center">
+          <FaLink size="80px" />
+          <h2 className="text-black m-3 text-7xl font-bold">LyricLink</h2>
+          </div>
+           <button className="w-32 h-12 border-none bg-black absolute top-2/4 text-white rounded-xl" onClick={handleLogin}>Login to Spotify</button>
          </div>
          </div>
       )}
