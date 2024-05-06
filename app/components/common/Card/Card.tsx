a9b68adb147591bb3ab234596545deb33556e05f
aman1357 <amancorp04@gmail.com>
2024-05-06 12:06:17 +0530

    .

--- app/components/common/Card/Card.tsx
+++ app/components/common/Card/Card.tsx
@@ -9,11 +9,10 @@ interface CardProps {
     return (
       <div className="h-48 w-40 m-3 p-2 rounded overflow-hidden shadow-lg bg-white">
         <div className='w-full h-full'>
-        <img className="w-full" src={imageUrl} alt={title} />
-        <div className="">
-          <div className="font-bold text-xs  text-black inline-block">{title}</div>
-          
-        </div>
+          <img className="w-full" src={imageUrl} alt={title} />
+          <div className="">
+            <div className="font-bold text-xs  text-black inline-block">{title}</div>
+          </div>
         </div>
       </div>
     );
