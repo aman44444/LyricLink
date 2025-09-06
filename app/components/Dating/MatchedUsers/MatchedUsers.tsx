e6f72f63df6b526402fdd45227a72539683995b5
aman1357 <amancorp04@gmail.com>
2025-09-06 12:24:59 +0530

    bg image

--- app/components/Dating/MatchedUsers/MatchedUsers.tsx
+++ app/components/Dating/MatchedUsers/MatchedUsers.tsx
@@ -1,180 +1,221 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import { firestore } from "@/app/utils/firebase.config";
-import { collection, getDocs, DocumentData, doc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  DocumentData,
+  doc,
+  setDoc,
+} from "firebase/firestore";
 import { UserData } from "@/app/interface/types";
-import { fetchUserData, fetchTopArtists, fetchTopTracks } from "@/app/utils/spotifyAPI";
+import {
+  fetchUserData,
+  fetchTopArtists,
+  fetchTopTracks,
+} from "@/app/utils/spotifyAPI";
 import MatchedUsersSlider from "../MatchedUsersSlider/MatchedUsersSlider";
 
-
 const jaccardSimilarity = (set1: Set<string>, set2: Set<string>): number => {
-    const intersection = new Set([...set1].filter(x => set2.has(x)));
-    const union = new Set([...set1, ...set2]);
-    return intersection.size / union.size;
+  const intersection = new Set([...set1].filter((x) => set2.has(x)));
+  const union = new Set([...set1, ...set2]);
+  return intersection.size / union.size;
 };
 
-const MatchedUsers: React.FC<{ currentUserId: string }> = ({ currentUserId }) => {
-    const [users, setUsers] = useState<UserData[]>([]);
-    const [matchedUsers, setMatchedUsers] = useState<UserData[]>([]);
-    const [matchingStarted, setMatchingStarted] = useState(false);
-    const [noMatchFound, setNoMatchFound] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
+const MatchedUsers: React.FC<{ currentUserId: string }> = ({
+  currentUserId,
+}) => {
+  const [users, setUsers] = useState<UserData[]>([]);
+  const [matchedUsers, setMatchedUsers] = useState<UserData[]>([]);
+  const [matchingStarted, setMatchingStarted] = useState(false);
+  const [noMatchFound, setNoMatchFound] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-
-    
-        const fetchUsers = async () => {
-            try {
-                const usersCollection = collection(firestore, "users");
-                const usersSnapshot = await getDocs(usersCollection);
-                const usersData: UserData[] = usersSnapshot.docs.map(doc => {
-                    const data = doc.data() as DocumentData;
-                    return {
-                        id: doc.id,
-                        display_name: data.userData.display_name,
-                        images: data.userData.images,
-                        topArtists: data.topArtists,
-                        topTracks: data.topTracks,
-                    };
-                });
-                console.log("Fetched users data:", usersData);
-                setUsers(usersData);
-            } catch (error) {
-                console.error("Error fetching user data from Firestore:", error);
-            }
+  const fetchUsers = async () => {
+    try {
+      const usersCollection = collection(firestore, "users");
+      const usersSnapshot = await getDocs(usersCollection);
+      const usersData: UserData[] = usersSnapshot.docs.map((doc) => {
+        const data = doc.data() as DocumentData;
+        return {
+          id: doc.id,
+          display_name: data.userData.display_name,
+          images: data.userData.images,
+          topArtists: data.topArtists,
+          topTracks: data.topTracks,
         };
+      });
+      console.log("Fetched users data:", usersData);
+      setUsers(usersData);
+    } catch (error) {
+      console.error("Error fetching user data from Firestore:", error);
+    }
+  };
 
-       
-      useEffect(() => {
-        fetchUsers();
-    }, []);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-    const handleStartMatching = async () => {
-        setIsLoading(true);
-        try {
-            const [userData, topArtists, topTracks] = await Promise.all([fetchUserData(), fetchTopArtists(), fetchTopTracks()]);
-            const userRef = doc(collection(firestore, "users"), userData.id);
-            await setDoc(userRef, {
-                userData,
-                topArtists,
-                topTracks,
-            });
-            setMatchingStarted(true);
-            await fetchUsers();
-        } catch (error) {
-            console.error("Error starting matching process:", error);
-        } finally {
-            setIsLoading(false);
-        }
+  const handleStartMatching = async () => {
+    setIsLoading(true);
+    try {
+      const [userData, topArtists, topTracks] = await Promise.all([
+        fetchUserData(),
+        fetchTopArtists(),
+        fetchTopTracks(),
+      ]);
+      const userRef = doc(collection(firestore, "users"), userData.id);
+      await setDoc(userRef, {
+        userData,
+        topArtists,
+        topTracks,
+      });
+      setMatchingStarted(true);
+      await fetchUsers();
+    } catch (error) {
+      console.error("Error starting matching process:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // const matchUsers = () => {
+    //     if (!matchingStarted || users.length === 0) return;
+
+    //     const threshold = 0.1;
+    //     const matches: UserData[] = [];
+
+    //     for (let i = 0; i < users.length; i++) {
+    //        if (users[i].id === currentUserId) continue;
+    //         const user1 = users[i];
+    //         const user1Artists = new Set(user1.topArtists.map((artist: any) => artist.name));
+    //         const user1Tracks = new Set(user1.topTracks.map((track: any) => track.name));
+
+    //         let matched = false;
+
+    //         for (let j = 0; j < users.length; j++) {
+    //             if (i === j) continue;
+
+    //             const user2 = users[j];
+    //             const user2Artists = new Set(user2.topArtists.map((artist: any) => artist.name));
+    //             const user2Tracks = new Set(user2.topTracks.map((track: any) => track.name));
+
+    //             const artistSimilarity = jaccardSimilarity(user1Artists, user2Artists);
+    //             const trackSimilarity = jaccardSimilarity(user1Tracks, user2Tracks);
+
+    //             if (artistSimilarity >= threshold || trackSimilarity >= threshold) {
+    //                 matched = true;
+    //                 console.log("Match found:");
+    //                 console.log("User 1:", user1);
+    //                 console.log("User 2:", user2);
+    //                 break;
+    //             }
+    //         }
+
+    //         if (matched && !matches.some(match => match.id === user1.id)) {
+    //             matches.push(user1);
+    //         }
+    //     }
+
+    //     if (matches.length === 0) {
+    //         setNoMatchFound(true);
+    //     } else {
+    //         setNoMatchFound(false);
+    //     }
+
+    //     setMatchedUsers(matches);
+    // };
+    const matchUsers = () => {
+      if (!matchingStarted || users.length === 0) return;
+
+      const threshold = 0.1;
+      const matches: UserData[] = [];
+
+      users.forEach((user1) => {
+        if (user1.id === currentUserId) return;
+
+        const user1Artists = new Set(
+          user1.topArtists.map((artist: any) => artist.name)
+        );
+        const user1Tracks = new Set(
+          user1.topTracks.map((track: any) => track.name)
+        );
+
+        let matched = false;
+
+        users.forEach((user2) => {
+          if (user1.id === user2.id) return;
+
+          const user2Artists = new Set(
+            user2.topArtists.map((artist: any) => artist.name)
+          );
+          const user2Tracks = new Set(
+            user2.topTracks.map((track: any) => track.name)
+          );
+
+          const artistSimilarity = jaccardSimilarity(
+            user1Artists,
+            user2Artists
+          );
+          const trackSimilarity = jaccardSimilarity(user1Tracks, user2Tracks);
+
+          if (artistSimilarity >= threshold || trackSimilarity >= threshold) {
+            matched = true;
+            console.log("Match found between users:");
+            console.log("User 1:", user1);
+            console.log("User 2:", user2);
+            if (!matches.some((match) => match.id === user1.id)) {
+              matches.push(user1);
+            }
+          }
+        });
+      });
+
+      if (matches.length === 0) {
+        setNoMatchFound(true);
+      } else {
+        setNoMatchFound(false);
+      }
+
+      setMatchedUsers(matches);
     };
-    
-    useEffect(() => {
-        // const matchUsers = () => {
-        //     if (!matchingStarted || users.length === 0) return;
-        
-        //     const threshold = 0.1;
-        //     const matches: UserData[] = [];
-        
-        //     for (let i = 0; i < users.length; i++) {
-        //        if (users[i].id === currentUserId) continue;
-        //         const user1 = users[i];
-        //         const user1Artists = new Set(user1.topArtists.map((artist: any) => artist.name));
-        //         const user1Tracks = new Set(user1.topTracks.map((track: any) => track.name));
-        
-        //         let matched = false;
-        
-        //         for (let j = 0; j < users.length; j++) {
-        //             if (i === j) continue;
-        
-        //             const user2 = users[j];
-        //             const user2Artists = new Set(user2.topArtists.map((artist: any) => artist.name));
-        //             const user2Tracks = new Set(user2.topTracks.map((track: any) => track.name));
-        
-        //             const artistSimilarity = jaccardSimilarity(user1Artists, user2Artists);
-        //             const trackSimilarity = jaccardSimilarity(user1Tracks, user2Tracks);
-        
-        //             if (artistSimilarity >= threshold || trackSimilarity >= threshold) {
-        //                 matched = true;
-        //                 console.log("Match found:");
-        //                 console.log("User 1:", user1);
-        //                 console.log("User 2:", user2);
-        //                 break;
-        //             }
-        //         }
-        
-        //         if (matched && !matches.some(match => match.id === user1.id)) {
-        //             matches.push(user1);
-        //         }
-        //     }
-        
-        //     if (matches.length === 0) {
-        //         setNoMatchFound(true);
-        //     } else {
-        //         setNoMatchFound(false);
-        //     }
-        
-        //     setMatchedUsers(matches);
-        // };
-        const matchUsers = () => {
-            if (!matchingStarted || users.length === 0) return;
-        
-            const threshold = 0.1;
-            const matches: UserData[] = [];
-        
-            users.forEach(user1 => {
-                if (user1.id === currentUserId) return;
-        
-                const user1Artists = new Set(user1.topArtists.map((artist: any) => artist.name));
-                const user1Tracks = new Set(user1.topTracks.map((track: any) => track.name));
-        
-                let matched = false;
-        
-                users.forEach(user2 => {
-                    if (user1.id === user2.id) return;
-        
-                    const user2Artists = new Set(user2.topArtists.map((artist: any) => artist.name));
-                    const user2Tracks = new Set(user2.topTracks.map((track: any) => track.name));
-        
-                    const artistSimilarity = jaccardSimilarity(user1Artists, user2Artists);
-                    const trackSimilarity = jaccardSimilarity(user1Tracks, user2Tracks);
-        
-                    if (artistSimilarity >= threshold || trackSimilarity >= threshold) {
-                        matched = true;
-                        console.log("Match found between users:");
-                        console.log("User 1:", user1);
-                        console.log("User 2:", user2);
-                        if (!matches.some(match => match.id === user1.id)) {
-                            matches.push(user1);
-                        }
-                    }
-                });
-            });
-        
-            if (matches.length === 0) {
-                setNoMatchFound(true);
-            } else {
-                setNoMatchFound(false);
-            }
-        
-            setMatchedUsers(matches);
-        };
-        
 
-        matchUsers();
-    }, [matchingStarted, users,currentUserId]);
+    matchUsers();
+  }, [matchingStarted, users, currentUserId]);
 
-    return (
-        <div className="h-screen flex justify-center items-center w-full border-2 border-neutral-800  bg-black rounded-md sm:h-full" style={{ backgroundImage: "url('match3.jpg')" ,backgroundRepeat: "no-repeat", backgroundSize: "cover"}}>
-             {isLoading ? (
-            <p>Loading...</p>  
+  return (
+    <div
+      className=" h-screen flex justify-center items-center w-full border-2 border-neutral-800  bg-black rounded-md sm:h-full"
+      style={{
+        backgroundImage: "url('datingBg.jpg')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+     
+        {isLoading ? (
+          <p>Loading...</p>
         ) : !matchingStarted && !matchedUsers.length ? (
-            <button onClick={handleStartMatching} className="text-lime-50 w-32 h-8 flex border-none items-center justify-center bg-green-900 rounded-full">Start Matching</button>
+          
+            <button
+              onClick={handleStartMatching}
+              className="text-lime-50 w-32 h-8 flex border-none items-center 
+                       justify-center bg-green-900 rounded-full
+                       hover:bg-emerald-600 hover:border
+                      hover:border-zinc-700"
+            >
+              Start Matching
+            </button>
+        
         ) : matchedUsers.length ? (
-            <MatchedUsersSlider matchedUsers={matchedUsers} />
+          <MatchedUsersSlider matchedUsers={matchedUsers} />
         ) : noMatchFound ? (
-            <p>No matches found</p>
+          <p>No matches found</p>
         ) : null}
-        </div>
-    );
+      </div>
+    
+  );
 };
 
 export default MatchedUsers;
